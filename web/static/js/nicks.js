110adfd0cf4813b579d86ec883f79f54d16b4d54
Freika <iamfrey@gmail.com>
2016-09-25 20:31:02 +0700

    Add jquery.

--- web/static/js/nicks.js
+++ web/static/js/nicks.js
@@ -1,16 +1,30 @@
-$('#orc-male').click(function(e){
-  e.preventDefault();
+  $('#orc-male').click(function(e){
+    alert(1)
+  })
+  // e.preventDefault();
 
-  race = this.data("race")
-  game = this.data("game")
+  // race = this.data("race")
+  // game = this.data("game")
 
-  $.ajax({
-    type: "POST",
-    dataType : "json",
-    url: "/api/nicks/game_id=" + game + "&race_id=" + race + "sex=male",
-    async: true,
-    success: function(data) {
-      console.log(data)
-    }
-  });
-});
+  // $.ajax({
+  //   type: "POST",
+  //   dataType : "json",
+  //   url: "/api/nicks/game_id=" + game + "&race_id=" + race + "sex=male",
+  //   success: function(data) {
+  //     console.log(data)
+  //   }
+  // });
+
+// function french_female() {
+//   $('#generated').fadeOut(350, function() {
+//     $.ajax({
+//     url: 'scripts/french_female.php',
+//       success: function(html) {
+//       $("#generated").html(html);
+
+//       }
+//     });
+//   });
+//   $('#generated').fadeIn(350);
+
+//   };
