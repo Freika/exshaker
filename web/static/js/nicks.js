4ef0b7271df3a6f90b0b27595cb6b8b9db33b72c
Freika <iamfrey@gmail.com>
2016-09-25 22:28:09 +0700

    Implement nick generation with ajax.

--- web/static/js/nicks.js
+++ web/static/js/nicks.js
@@ -1,19 +1,18 @@
   $('#orc-male').click(function(e){
-    alert(1)
+    e.preventDefault();
+
+    var race = $(this).data("race")
+    var game = $(this).data("game")
+
+    $.ajax({
+      type: "POST",
+      dataType : "json",
+      url: "/api/nicks?game_id=" + game + "&race_id=" + race + "&sex=male",
+      success: function(data) {
+        $('#nickbox').html(data.nick.name)
+      }
+    });
   })
-  // e.preventDefault();
-
-  // race = this.data("race")
-  // game = this.data("game")
-
-  // $.ajax({
-  //   type: "POST",
-  //   dataType : "json",
-  //   url: "/api/nicks/game_id=" + game + "&race_id=" + race + "sex=male",
-  //   success: function(data) {
-  //     console.log(data)
-  //   }
-  // });
 
 // function french_female() {
 //   $('#generated').fadeOut(350, function() {
--- web/views/nick_view.ex
+++ web/views/nick_view.ex
@@ -1,12 +1,8 @@
 defmodule Exshaker.NickView do
   use Exshaker.Web, :view
 
-  def render("index.json", %{nicks: nicks}) do
-    %{data: render_many(nicks, Exshaker.NickView, "nick.json")}
-  end
-
   def render("show.json", %{nick: nick}) do
-    %{data: render_one(nick, Exshaker.NickView, "nick.json")}
+    %{nick: render_one(nick, Exshaker.NickView, "nick.json")}
   end
 
   def render("nick.json", %{nick: nick}) do
