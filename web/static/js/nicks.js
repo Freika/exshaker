bb23cb25a760a0a78221e37994d0da37bf656c57
Freika <iamfrey@gmail.com>
2016-09-26 23:43:55 +0700

    Add wow night elves race and syllables to seeds.

--- web/static/js/nicks.js
+++ web/static/js/nicks.js
@@ -1,29 +1,15 @@
-  $('#orc-male').click(function(e){
-    e.preventDefault();
+$('#orc-male').click(function(e){
+  e.preventDefault();
 
-    var race = $(this).data("race")
-    var game = $(this).data("game")
+  var race = $(this).data('race')
+  var game = $(this).data('game')
 
-    $.ajax({
-      type: "POST",
-      dataType : "json",
-      url: "/api/nicks?game_id=" + game + "&race_id=" + race + "&sex=male",
-      success: function(data) {
-        $('#nickbox').html(data.nick.name)
-      }
-    });
-  })
-
-// function french_female() {
-//   $('#generated').fadeOut(350, function() {
-//     $.ajax({
-//     url: 'scripts/french_female.php',
-//       success: function(html) {
-//       $("#generated").html(html);
-
-//       }
-//     });
-//   });
-//   $('#generated').fadeIn(350);
-
-//   };
+  $.ajax({
+    type: 'POST',
+    dataType : 'json',
+    url: '/api/nicks?game_id=' + game + '&race_id=' + race + '&sex=male',
+    success: function(data) {
+      $('#nickbox').html(data.nick.name)
+    }
+  });
+})
